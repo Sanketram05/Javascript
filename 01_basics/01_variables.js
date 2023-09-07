b884970110e793307bb709da4c6b02a65ac24cc8
Sanketram05 <118762883+Sanketram05@users.noreply.github.com>
2023-09-07 09:26:23 +0000

    02_basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 144553
+let accountEmail = "SamBRuh@google"
+var accPassword = "12345"
+accountCity = "MUMBAI"
+let accountState;
+
+// accountID = 2 // not allowed
+
+accountEmail = "sam@micro"
+accPassword = "89434"
+accountCity = "Bengaluru"
+
+console.log(accountID);
+console.table([accountID,accountEmail,accPassword,accountCity,accountState])
